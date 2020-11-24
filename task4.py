0354a9d154e0691cba41c9d43da76f661a649a68
Chirag Vartak <vartak.chirag.8@gmail.com>
2020-11-24 15:44:29 +0530

    Improve task 4 outputs

--- task4.py
+++ task4.py
@@ -14,21 +14,23 @@ NI_COUNTS = []
 RI_COUNTS = []
 
 
-def get_modified_results_after_probabilistic_feedback(gesture_name: str,
+def get_modified_results_after_probabilistic_feedback(query_gesture_name: str,
                                                       relevant_results: List,
                                                       irrelevant_results: List,
                                                       untagged_results: List):
     cleaned_relevant_result_names = [e.split('_')[0] for e in relevant_results]
-    df = utils.get_gesture_vectors(GESTURE_VECTORS_FILE_PATH)
-    bdf = _binarize(df)
-    print(df)
+    bdf = _binarize(utils.get_gesture_vectors(GESTURE_VECTORS_FILE_PATH))
+    # print(bdf)
     similarity_gesture_pairs = []
-    for gesture_index, gesture_row in df.iterrows():
-        similarity = sim(d=gesture_row, q=df.loc[gesture_name],
+    for relevant_gesture_name, relevant_gesture_vector in bdf.loc[relevant_results].iterrows():
+        similarity = sim(d=relevant_gesture_vector,
                          relevant_results=cleaned_relevant_result_names,
-                         bdf=bdf, odf=df,
-                         q_gesture_name=gesture_name, d_gesture_name=gesture_index)
-        similarity_gesture_pairs.append((similarity, gesture_index))
+                         bdf=bdf)
+        similarity_gesture_pairs.append((similarity, relevant_gesture_name))
+    global NI_COUNTS
+    NI_COUNTS = []  # Reset for re-running
+    global RI_COUNTS
+    RI_COUNTS = []
     return sorted(similarity_gesture_pairs)
 
 
@@ -54,18 +56,17 @@ def _binarize_column(s: Series):
     return pd.Series(labels, name=s.name, index=s.index)
 
 
-def sim(d: list, q: list, relevant_results: List, bdf: DataFrame, odf: DataFrame, q_gesture_name, d_gesture_name):
+def sim(d: list, relevant_results: List, bdf: DataFrame):
     R = len(relevant_results)
     N = len(bdf)
     n = count_of_objects_in_which_di_1(bdf)
     r = count_of_relevant_objects_in_which_di_1(relevant_results, bdf)
-    sum = 0
+    sum_value = 0
     for i in range(len(bdf.columns)):
         pi = (r[i] + 0.5) / (R + 1)
         ui = (n[i] - r[i] + 0.5) / (N - R + 1)
-        sum += d[i] * log(pi * (1 - ui) / ui * (1 - pi)) + \
-               10*np.dot(odf.loc[q_gesture_name], odf.loc[d_gesture_name])
-    return sum
+        sum_value += d[i] * log(pi*(1 - ui) / ui*(1 - pi))
+    return sum_value
 
 
 def count_of_objects_in_which_di_1(df: DataFrame):
@@ -75,15 +76,19 @@ def count_of_objects_in_which_di_1(df: DataFrame):
     for column_name in df.columns:
         entries_with_1 = df[df[column_name] == 1]
         NI_COUNTS.append(len(entries_with_1))
+        # print(df[df[column_name] == 1].index)
+        # print(df.index)
     print("NI_COUNTS", NI_COUNTS)
     return NI_COUNTS
 
 
 def count_of_relevant_objects_in_which_di_1(relevant_results: List, df: DataFrame):
+    print(relevant_results)
     global RI_COUNTS
     if len(RI_COUNTS) != 0:
         return RI_COUNTS
     relevant_df = df.loc[relevant_results]
+    print(relevant_df)
     for column_name in df.columns:
         entries_with_1 = relevant_df[relevant_df[column_name] == 1]
         RI_COUNTS.append(len(entries_with_1))
@@ -94,13 +99,13 @@ def count_of_relevant_objects_in_which_di_1(relevant_results: List, df: DataFram
 if __name__ == "__main__":
     pd.set_option('display.max_rows', 500)
     similarity_gesture_pairs = get_modified_results_after_probabilistic_feedback(
-        "271",
+        "570",
         # ["561", "562", "563", "563", "564", "565", "566", "567", "568", "560"],
         # [str(x) for x in range(559, 589+1)],
-        [str(x) for x in [250, 253, 258, 263, 265, 577, 582, 1]],
+        # [str(x) for x in [250, 253, 258, 263, 265, 577, 582, 1]],
         # [str(x) for x in range(1, 30 + 1)],
         # ["561"] * 10,
-        # ["571", "572", "573", "574", "575", "576"],
+        ["571", "572", "573", "574", "575", "576", "570", "570", "570", "570", "570", "570", "570", "570", "570", "570", "570"],
         [],
         []
     )
