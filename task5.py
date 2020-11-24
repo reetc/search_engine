33a5fcec2a01f06822b4a75d95dcb3cdcb74883a
varun <varunraoveeramaneni23@gmail.com>
2020-11-24 11:36:56 -0700

    made changes to the structure

--- task5.py
+++ task5.py
@@ -1,14 +1,18 @@
 import pandas as pd
 import numpy as np
 from utils import *
-import math
+from math import sqrt
+import argparse
+from typing import List
 gestureMatrix = pd.DataFrame.empty
 componentMatrix = pd.DataFrame.empty
+GESTURE_VECTORS_FILE_PATH = "latent_features.csv"
+SIMILARITY_MATRIX_FILE_PATH = "SVD_50_similarity_matrix.csv"
 
 
-def initComponentMatrix():
+def initComponentMatrix(gesture_vectors_file_path):
     global componentMatrix
-    componentMatrix = pd.read_csv("latent_features.csv",index_col=0)
+    componentMatrix = pd.read_csv(gesture_vectors_file_path,index_col=0)
     new_newindex = []
     for ind in componentMatrix.index.to_list():
         split_ind = ind.split('_')
@@ -63,9 +67,9 @@ def getSimilarityMatrix(gestureMatrix: pd.DataFrame, initialQuery : str):
     return nayanew_similarity_matrix
 
 
-def get_similarity_matrix(initialQuery):
+def get_similarity_matrix(similarity_matrix_path,initialQuery):
     global gestureMatrix
-    gestureMatrix = pd.read_csv("SVD_50_similarity_matrix.csv",index_col=0)
+    gestureMatrix = pd.read_csv(similarity_matrix_path,index_col=0)
     # print(gestureMatrix.iloc[initialQuery].shape)
     # Di = gestureMatrix.iloc[initialQuery]
     similarityMatrix = getSimilarityMatrix(gestureMatrix, initialQuery)
@@ -74,39 +78,8 @@ def get_similarity_matrix(initialQuery):
 def getResult(pagerank, m=10):
     return pagerank[:m]
 
-
-
 def modifypagerankvector(page_rank_seed_vector,relavant,irrelavant,untouched):
     global componentMatrix
-
-    # for index,value in componentMatrix.iteritems():
-    #     if index in relavant:
-    #         componentsList = componentMatrix.loc[index]
-    #         rel=index
-    #         toAdd = 0
-    #         denominator = 0
-    #         for val in componentsList:
-    #             val = float(val)
-    #             toAdd = toAdd + abs(val)
-    #             denominator = denominator + val ** 2
-    #         print("todadd, denominatio", toAdd,denominator)
-    #         page_rank_seed_vector.loc[index] = float(page_rank_seed_vector.loc[index]) + (toAdd / math.sqrt(denominator))
-    #         page_rank_seed_vector.loc[index] = float(page_rank_seed_vector.loc[index])/ len(relavant)
-    #
-    # for index,value in componentMatrix.iteritems():
-    #     componentsList=[]
-    #     if index in irrelavant:
-    #         componentsList = componentMatrix.loc[index]
-    #         toSub = 0
-    #         irrel=index
-    #         denominator = 0
-    #         for val in componentsList:
-    #             val = float(val)
-    #             toSub = toSub + val
-    #             denominator = denominator + val ** 2
-    #         page_rank_seed_vector.loc[index] = page_rank_seed_vector.loc[index] - (toSub / math.sqrt(denominator))
-    #         page_rank_seed_vector.loc[index] = page_rank_seed_vector.loc[index] / len(irrelavant)
-
     for rel in relavant:
         componentsList = componentMatrix.loc[rel]
         toAdd = 0
@@ -114,9 +87,7 @@ def modifypagerankvector(page_rank_seed_vector,relavant,irrelavant,untouched):
         for val in componentsList:
             toAdd = toAdd + val
             denominator =denominator + val**2
-        print("toadd",toAdd)
-        print("denominator",denominator)
-        page_rank_seed_vector.loc[rel] = page_rank_seed_vector.loc[rel] + (toAdd / math.sqrt(denominator))
+        page_rank_seed_vector.loc[rel] = page_rank_seed_vector.loc[rel] + (toAdd / sqrt(denominator))
         page_rank_seed_vector.loc[rel] = page_rank_seed_vector.loc[rel] / len(relavant)
     for irrel in irrelavant:
         componentsList = componentMatrix.loc[irrel]
@@ -125,27 +96,61 @@ def modifypagerankvector(page_rank_seed_vector,relavant,irrelavant,untouched):
         for val in componentsList:
             toSub = toSub + abs(val)
             denominator =denominator + val**2
-        page_rank_seed_vector.loc[irrel] = page_rank_seed_vector.loc[irrel] - (toSub / math.sqrt(denominator))
+        page_rank_seed_vector.loc[irrel] = page_rank_seed_vector.loc[irrel] - (toSub / sqrt(denominator))
         page_rank_seed_vector.loc[irrel] = page_rank_seed_vector.loc[irrel] / len(irrelavant)
     # print("pagerankseed vector",page_rank_seed_vector)
     return page_rank_seed_vector
 
-if __name__ == "__main__":
-    initComponentMatrix()
-    similarity_matrix = get_similarity_matrix("1-5")
+
+def get_modified_results_after_ppr(query_gesture_name: str, relevant_results: List, irrelevant_results: List, untagged_results: List,
+                                                      gesture_vectors_file_path=GESTURE_VECTORS_FILE_PATH,
+                                   similarity_matrix_path = SIMILARITY_MATRIX_FILE_PATH):
+    initComponentMatrix(gesture_vectors_file_path)
+    similarity_matrix = get_similarity_matrix(similarity_matrix_path,query_gesture_name)
     page_rank_seed_vector = initialSeedMatrix()
-
-
-    #page_rank_seed_vector=np.asarray(page_rank_seed_vector)
-    #print(page_rank_seed_vector)
-    page_rank_seed_vector=modifypagerankvector(page_rank_seed_vector,["1-1","2-3","3-4","4-5"],["250-1","251-2","252-3"],[])
-    # print("final seed index",page_rank_seed_vector.index)
-    page_rank_seed_vector =np.asarray(page_rank_seed_vector)
-    # print("final seed",page_rank_seed_vector.shape)
-    # print("final seed",page_rank_seed_vector)
-    # print("final simil",similarity_matrix)
+    print("relevant result list",relevant_results)
+    page_rank_seed_vector = modifypagerankvector(page_rank_seed_vector, relevant_results,
+                                                 irrelevant_results, untagged_results)
+    page_rank_seed_vector = np.asarray(page_rank_seed_vector)
     page_rank_values = page_rank(similarity_matrix, page_rank_seed_vector, 100, 0.85)
-    # print("pagerankkvalues",page_rank_values)
     result = getResult(page_rank_values)
-    print("result",result)
+    return result
+
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-q", "--query", help="list of relevant gestures, e.g. --query=570")
+    parser.add_argument("-r", "--relevant", help="list of relevant gestures, e.g. --relevant=570,570,571,574,575")
+    parser.add_argument("-i", "--irrelevant", help="list of irrelevant gestures, e.g. --irrelevant=572,573")
+    args = parser.parse_args()
+
+    if args.query is None:
+        print("-q or --query argument missing")
+        exit(0)
+    query = args.query
+
+    if args.relevant is None:
+        print("-r or --relevant argument missing")
+        exit(0)
+    relevant = args.relevant.split(',')
+
+    if args.irrelevant is None:
+        print("-i or --irrelevant argument missing")
+        exit(0)
+    irrelevant = args.irrelevant.split(',')
+
+
+    new_suggestions = get_modified_results_after_ppr(query_gesture_name=query,
+                                                     relevant_results=relevant,
+                                                     irrelevant_results=irrelevant,
+                                                     untagged_results=[],
+                                                     gesture_vectors_file_path=GESTURE_VECTORS_FILE_PATH,
+                                                     similarity_matrix_path = SIMILARITY_MATRIX_FILE_PATH)
+
+    print(new_suggestions)
+
+
+
 
