b8b3d882c0130ba92c1a9a53b3e87fcf1ba0c07e
BoiceWong <bwong8@asu.edu>
2020-11-24 00:01:58 -0700

    Task 6 Handles both Task 4 and 5: However, incomplete

--- task6.py
+++ task6.py
@@ -1,10 +1,12 @@
 
 import argparse
+import task4
 import task5
 import task3
 from task3 import LSH,HTable
 from task1 import predict_custom
 import pandas
+from pprint import pprint
 
 
 def import_data():
@@ -51,28 +53,54 @@ data,file_order=task5.import_data()
 query_vec = data[file_order.index(query+"_vector_tfidf.txt")]
 
 # print(file_order,query_vec)
-while(1):
 
-    new_query=task5.modify_query_vec(relevant_gestures,nonrelevant_gestures,query_vec)
-    print(new_query)
-    res = task3.predict_custom(new_query,t)
-    # res = predict_custom(new_query,t)
-    for el in res:
-        print("Gesture:",el[1],"Score:",el[0])
+#task 4
+if option == 1:
+    while(1):
 
-    input_str = input("Enter q to quit: ")
-    if input_str == "q" or input_str == "Q":
-        exit(0)
-    # while(1):
-    input_str = input("Enter comma seperated relevant file keys: ")
-    relevant_gestures = input_str.split(",")
-    print("Relevant:",relevant_gestures)
+        new_similarity_gesture_pairs=task4.get_modified_results_after_probabilistic_feedback(query, relevant_gestures, nonrelevant_gestures, untagged_results=[])
+        print("Outputs:")
+        pprint(new_similarity_gesture_pairs)
 
-    input_str = input("Enter comma seperated nonn-relevant file keys: ")
-    nonrelevant_gestures = input_str.split(",")
-    print("Non-Relevant:",nonrelevant_gestures)
+        input_str = input("Enter q to quit: ")
+        if input_str == "q" or input_str == "Q":
+            exit(0)
+        # while(1):
+        input_str = input("Enter comma seperated relevant file keys: ")
+        relevant_gestures = input_str.split(",")
+        print("Relevant:", relevant_gestures)
 
-    query_vec = new_query
-    print(query_vec)
-    # break
->>>>>>> main
+        input_str = input("Enter comma seperated nonn-relevant file keys: ")
+        nonrelevant_gestures = input_str.split(",")
+        print("Non-Relevant:", nonrelevant_gestures)
+
+        #query_vec = new_query
+        #print(query_vec)
+        # break
+
+#task 5
+elif option == 2:
+    while(1):
+
+        new_query=task5.modify_query_vec(relevant_gestures,nonrelevant_gestures,query_vec)
+        print(new_query)
+        res = task3.predict_custom(new_query,t)
+        # res = predict_custom(new_query,t)
+        for el in res:
+            print("Gesture:",el[1],"Score:",el[0])
+
+        input_str = input("Enter q to quit: ")
+        if input_str == "q" or input_str == "Q":
+            exit(0)
+        # while(1):
+        input_str = input("Enter comma seperated relevant file keys: ")
+        relevant_gestures = input_str.split(",")
+        print("Relevant:",relevant_gestures)
+
+        input_str = input("Enter comma seperated nonn-relevant file keys: ")
+        nonrelevant_gestures = input_str.split(",")
+        print("Non-Relevant:",nonrelevant_gestures)
+
+        query_vec = new_query
+        print(query_vec)
+        # break
